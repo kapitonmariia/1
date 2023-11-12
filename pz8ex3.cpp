cb2bfc9c6e9e6bba20c56f9d9fb7f91a82573618
kapitonmariia <128748488+kapitonmariia@users.noreply.github.com>
2023-11-12 17:00:32 +0200

    pz8ex3.cpp

--- pz8ex3.cpp
+++ pz8ex3.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    const int size = 5; // Set the size of the array
+    const int size = 5; 
     int arr[size];
 
-    // User input to fill the array
-    std::cout << "Enter " << size << " numbers:\n";
+    cout << "Enter " << size << " numbers:\n";
     for (int i = 0; i < size; ++i) {
-        std::cin >> arr[i];
+        cin >> arr[i];
     }
 
-    // Find the indices of the minimum and maximum elements
     int minIndex = 0, maxIndex = 0;
     for (int i = 1; i < size; ++i) {
         if (arr[i] < arr[minIndex]) {
@@ -21,17 +20,15 @@ int main() {
         }
     }
 
-    // Swap values between the minimum and maximum elements
     int temp = arr[minIndex];
     arr[minIndex] = arr[maxIndex];
     arr[maxIndex] = temp;
 
-    // Print the resulting array
-    std::cout << "Array after swapping:\n";
+    cout << "Array after swapping:\n";
     for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
+        cout << arr[i] << " ";
     }
-    std::cout << std::endl;
+    cout << std::endl;
 
     return 0;
 }
