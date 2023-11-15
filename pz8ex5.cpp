6ec282653bc17bf672ebe40cb025cfb06b9b2000
kapitonmariia <128748488+kapitonmariia@users.noreply.github.com>
2023-11-15 15:35:08 +0200

    pz8ex5.cpp

--- pz8ex5.cpp
+++ pz8ex5.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    const int maxSize = 100; 
-    int arr[maxSize];
-    int size;
+    const int Size = 5;
+    int numbers[Size];
 
-    cout << "Enter the size of the array: ";
-    cin >> size;
+    srand(time(0));
 
-    if (size > maxSize) {
-        cout << "Error: Size exceeds the maximum allowed size.\n";
-        return 1; 
+    for (int i = 0; i < Size; ++i) {
+        numbers[i] = rand() % 100; 
     }
 
-    cout << "Enter " << size << " numbers:\n";
-    for (int i = 0; i < size; ++i) {
-        cin >> arr[i];
+    cout << "The original array: ";
+    for (int i = 0; i < Size; ++i) {
+        cout << numbers[i] << " ";
     }
+    cout << endl;
 
-    int maxElement = arr[0];
-    int secondMaxElement = arr[0];
+    
+    sort(numbers, numbers + Size, greater<int>());
 
-    for (int i = 1; i < size; ++i) {
-        if (arr[i] > maxElement) {
-            secondMaxElement = maxElement;
-            maxElement = arr[i];
-        } else if (arr[i] > secondMaxElement && arr[i] < maxElement) {
-            secondMaxElement = arr[i];
-        }
-    }
+    int secondLargest = numbers[1];
 
-    cout << "Second largest element: " << secondMaxElement << endl;
+    cout << "The second bigger number: " << secondLargest << endl;
 
     return 0;
 }
