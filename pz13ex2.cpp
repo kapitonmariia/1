08199d7b8ae48e0da47116801850dd0b9bf09b92
kapitonmariia <128748488+kapitonmariia@users.noreply.github.com>
2023-12-06 16:29:43 +0200

    pz13ex2.cpp

--- pz13ex2.cpp
+++ pz13ex2.cpp
@@ -1,34 +1,59 @@
-#include <iostream> 
-using namespace std; 
- 
-struct Date { 
-    int hours; 
-    int minutes; 
-}; 
- 
-int main() { 
-    Date startTime, endTime; 
- 
-    cout << "Enter the start time of the event: "; 
-    cin >> startTime.hours; 
- 
-    cout << "Enter the minutes of the event start: "; 
-    cin >> startTime.minutes; 
- 
-    cout << "Enter the end time of the event:"; 
-    cin >> endTime.hours; 
- 
-    cout << "Enter the minutes of the event completion:"; 
-    cin >> endTime.minutes; 
- 
-    int startTotalMinutes = startTime.hours * 60 + startTime.minutes; 
-    int endTotalMinutes = endTime.hours * 60 + endTime.minutes; 
-    int durationTotalMinutes = endTotalMinutes - startTotalMinutes; 
- 
-    int durationHours = durationTotalMinutes / 60; 
-    int durationMinutes = durationTotalMinutes % 60; 
- 
-    cout << "Duration of the event: " << durationHours << "hours " << durationMinutes << "minutes" << endl; 
- 
-    return 0; 
+#include <iostream>
+using namespace std;
+
+struct Date {
+    int hours;
+    int minutes;
+};
+
+int main() {
+    Date startTime, endTime;
+
+    cout << "Enter the hours of the starting time: ";
+    cin >> startTime.hours;
+
+    while (startTime.hours < 0 || startTime.hours > 23) {
+        cout << "Invalid input! Enter the hours again (0-23): ";
+        cin >> startTime.hours;
+    }
+
+    cout << "Enter the minutes of the starting time: ";
+    cin >> startTime.minutes;
+
+    while (startTime.minutes < 0 || startTime.minutes > 59) {
+        cout << "Invalid input! Enter the minutes again (0-59): ";
+        cin >> startTime.minutes;
+    }
+
+    cout << "Enter the hours of the ending time: ";
+    cin >> endTime.hours;
+
+    while (endTime.hours < 0 || endTime.hours > 23) {
+        cout << "Invalid input! Enter the hours again (0-23): ";
+        cin >> endTime.hours;
+    }
+
+    cout << "Enter the minutes of the ending time: ";
+    cin >> endTime.minutes;
+
+    while (endTime.minutes < 0 || endTime.minutes > 59) {
+        cout << "Invalid input! Enter the minutes again (0-59): ";
+        cin >> endTime.minutes;
+    }
+
+    int totalStartMinutes = startTime.hours * 60 + startTime.minutes;
+    int totalEndMinutes = endTime.hours * 60 + endTime.minutes;
+    int durationMinutes = (totalEndMinutes - totalStartMinutes + 24 * 60) % (24 * 60);
+
+    Date durationTime;
+    durationTime.hours = durationMinutes / 60;
+    durationTime.minutes = durationMinutes % 60;
+
+    cout << "Event duration: ";
+    if (durationTime.hours < 10) cout << "0";
+    cout << durationTime.hours << ":";
+    if (durationTime.minutes < 10) cout << "0";
+    cout << durationTime.minutes << endl;
+
+    return 0;
 }
