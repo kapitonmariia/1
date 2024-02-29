994c078f51d338ed6f34c7f7617976a88aed6af2
kapitonmariia <128748488+kapitonmariia@users.noreply.github.com>
2024-02-29 23:52:27 +0200

    sem2 pz1.cpp

--- sem2 pz1.cpp
+++ sem2 pz1.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    int vector[] = { 3, -5, 7, 10, -4, 14, 5, 2, -13 };
-    int n = sizeof(vector) / sizeof(vector[0]);
-    
-    int *ptr = vector; 
-    int min_element = *ptr; 
-    int max_element = *ptr; 
-    
-    for(int i = 1; i < n; i++) {
-        if(*(ptr + i) < min_element) { 
-            min_element = *(ptr + i); 
-        }
-        if(*(ptr + i) > max_element) { 
-            max_element = *(ptr + i); 
-        }
-    }
-    
-    cout << "Minimum element: " << min_element << endl;
-    cout << "Maximum element: " << max_element << endl;
-    
-    return 0;
+     int vector[] = { 3, -5, 7, 10, -4, 14, 5, 2, -13 };
+     int n = sizeof(vector) / sizeof(vector[0]);
+
+     int* ptr = vector;
+
+     int maxElement = *ptr;
+
+     for (int i = 1; i < n; ++i) {
+         ptr++;
+         if (*ptr > maxElement) {
+             maxElement = *ptr;
+         }
+     }
+
+     cout << "Maximum element: " << maxElement << endl;
+
+     return 0;
 }
